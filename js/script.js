059b10b777308bb7a60cdeb80363565d9367a0f4
Juffin <juffinhulley@gmail.com>
2022-05-20 10:48:24 +0300

    made tabs

--- js/script.js
+++ js/script.js
@@ -1,22 +1,27 @@
-/* $(document).ready(function () {
+$(document).ready(function () {
     $('.carousel__inner').slick({
         speed: 1200,
         // adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                dots: true,
+                arrows: false
             }
-        ]
+        }]
     });
-}); */
 
-const slider = tns({
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+});
+
+/* const slider = tns({
     container: '.carousel__inner',
     items: 1,
     slideBy: 'page',
@@ -30,4 +35,4 @@ document.querySelector('.prev').addEventListener('click', function () {
 });
 document.querySelector('.next').addEventListener('click', function () {
     slider.goTo('next');
-});
+}); */
